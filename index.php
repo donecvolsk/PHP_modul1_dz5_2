6ce642e0c71e9f29eb7d1eb63dd09d13137e585a
donecvolsk <donecvolsk@yandex.ru>
2024-10-11 16:23:22 +0400

    Добавлена функция логики для первой недели

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-$month = 11; //порядковый номер месяца в году
+$month = 10; //порядковый номер месяца в году
 $year = 24; //номер года
 
 function getCalendarForMonth(int $month, int $year): void
@@ -31,6 +31,20 @@ function getCalendarForMonth(int $month, int $year): void
         }
     }
 
+    // перебор и вывод дней первой недели месяца
+    function firstWeekDay(array $array): void {
+        foreach ($array as $value) 
+        {
+            if($value === '01' && (array_search($value, $array)) != 6 && (array_search($value, $array)) 
+                != 7 || $value === '04' && (array_search($value, $array)) != 6 && (array_search($value, $array)) != 7) {
+                echo "\033[31m$value\033[0m" . ' ' ;
+            } else {
+                echo $value . ' ';
+            };
+
+        }
+        
+    }
     function workingDays(array $array): void 
     {
         foreach ($array as $value) {
@@ -45,7 +59,7 @@ function getCalendarForMonth(int $month, int $year): void
     //Вывод календаря в консоль
     echo date('F', mktime(0, 0, 0, $month, 1, $year)) . PHP_EOL;
     echo 'Пн' . ' ' . 'Вт' . ' ' . 'Ср' . ' ' . 'Чт' . ' ' . 'Пт' . ' ' . 'Сб' . ' ' . 'Вс' . PHP_EOL; 
-    workingDays($arMonthDay[1]);
+    firstWeekDay($arMonthDay[1]);
     echo PHP_EOL; 
     workingDays($arMonthDay[2]);
     echo PHP_EOL; 
